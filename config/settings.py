973d68908fa2e8c0a4b10cd860e047fb0d50195d
Cyrille Limousin <cyrille.limousin@gmail.com>
2026-04-20 18:51:18 +0200

    force rebuild static

--- config/settings.py
+++ config/settings.py
@@ -86,10 +86,7 @@ USE_TZ = True
 
 # --- Static ---
 STATIC_URL = '/static/'
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # =========================================================
 # ✅ CONFIG PROD (Railway / HTTPS / CSRF)
